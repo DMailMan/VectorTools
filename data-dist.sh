0699ba347801b19ff02687e570d07621a120ca92
David Postle <dave.postle@gmail.com>
2015-09-09 11:57:05 +0100

    Minor changes to clarify comments and purpose

--- data-dist.sh
+++ data-dist.sh
@@ -13,12 +13,13 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-# This script is designed to report information about the Vector (or Vector-H) Mix Max indexes.
-# These indexes indicate the order in which data is stored on disk, and are important to query
-# performance.
 
 # This SQL script is intended to show the numbers of rows stored in each partition of a Vector/H table.
 # Pass in the database name and table name that you want to examine as the only parameters needed.
+
+# The number of rows or blocks in each partition of a table should be roughly equal. If not, then this data 
+# skew will result in uneven execution time of queries and uneven usage of a cluster, depending on which 
+# data is accessed.
 
 # You can also use vwinfo -T to get the same data in terms of disk blocks, rather than rows.
 
@@ -29,7 +30,7 @@ then
 fi
 
 sql $1 <<EOF
-SELECT tid/10000000000000000 AS partition_id, COUNT(*) AS num_tuples 
+SELECT tid/10000000000000000 AS partition_id, COUNT(*) AS num_rows 
 FROM $2
 GROUP BY 1 ORDER BY 1;\g
 EOF
